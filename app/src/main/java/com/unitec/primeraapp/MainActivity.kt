8f3f32c0df06f354b6a608b30ff9f0c525e16795
viviana <viviana_021297@hotmail.com>
2019-09-24 19:42:35 -0500

    Primer

---
+++ app/src/main/java/com/unitec/primeraapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.unitec.primeraapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Quitamos la action bar que aparece de defecto
+        actionBar?.hide()
+    }
+}
