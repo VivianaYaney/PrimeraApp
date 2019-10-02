d3b5bfe3d711136322d39d5323104d0e9617ecfe
SE <viviana_021297@hotmail.com>
2019-10-01 19:35:25 -0500

    Musiquita Time

--- app/src/main/java/com/unitec/primeraapp/BienvenidoActivity.kt
+++ app/src/main/java/com/unitec/primeraapp/BienvenidoActivity.kt
@@ -1,10 +1,12 @@
 package com.unitec.primeraapp
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 
 import kotlinx.android.synthetic.main.activity_bienvenido.*
+import kotlinx.android.synthetic.main.content_bienvenido.*
 
 class BienvenidoActivity : AppCompatActivity() {
 
@@ -14,7 +16,17 @@ class BienvenidoActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            var sonido=MediaPlayer.create(applicationContext, R.raw.feliz)
+            //Se inicia el sonido
+            sonido.start()
+            var sonido2=MediaPlayer.create(applicationContext, R.raw.sad)
+            //Se inicia el sonido
+            sonido2.start()
+            var peso= txtpeso.text.toString().toFloat()
+            var altura= txtaltura.text.toString().toFloat()
+            var imc = Milmc()
+
+            Snackbar.make(view, "Tu peso es $peso tu altura es $altura Tu IMC es ${imc.calcular(peso, altura)}", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
